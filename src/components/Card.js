69c8f8d924ba847bcd9d32f8efeeb01c8b412bf4
pratikshasamane <pratikshas@bsf.io>
2022-03-01 11:41:25 +0530

    Comment added

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,6 +2,6 @@ import './Card.css';
 
 function Card( props ){
     const classes = 'card ' + props.className;
-    return <div className={classes}>{props.children}</div>;
+    return <div className={classes}>{props.children}</div>; // this is composition.
 }
 export default Card;
